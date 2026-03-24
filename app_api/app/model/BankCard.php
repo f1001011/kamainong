04e430882061ac8a1bb8446a56770e4bf99c93c1
李斐济 <f1001012@outlook.com>
2026-03-24 09:42:03 +0800

    提交1

---
+++ app_api/app/model/BankCard.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\model;
+
+use think\Model;
+
+class BankCard extends Model
+{
+    protected $name = 'common_bank_card';
+}
---
+++ app_api/app/model/Withdraw.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\model;
+
+use think\Model;
+
+class Withdraw extends Model
+{
+    protected $name = 'common_pay_cash';
+}
